8cc85bfab9830e38f3876e7cbf891cfcaa9cc26a
aryan shukla <aryanshukla212004@gmail.com>
2025-04-20 13:48:03 +0530

    added new port

--- config/settings/local.py
+++ config/settings/local.py
@@ -13,6 +13,7 @@ CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
     "http://192.168.1.102:8081",  # Frontend IP address
     "http://192.168.1.103:8081",  # Frontend IP address
+    "http://192.168.1.104:8081",  # Frontend IP address
 ]
 
 # CACHES
